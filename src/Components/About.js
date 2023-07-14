45923b8c7766c3db66a76ed0fbcff7626892e829
Prerith Shetty <prerithshetty6@gmail.com>
2023-07-14 16:15:39 +0530

    done

---
+++ src/Components/About.js
@@ -0,0 +1,9 @@
+import React from "react";
+const About = () => {
+ return(
+    <>
+      <h1>contact page</h1>
+    </>
+ );
+};
+export default About;
---
+++ src/Components/Course.js
@@ -0,0 +1,9 @@
+import React from "react";
+const Course = () => {
+ return(
+    <>
+      <h1>contact page</h1>
+    </>
+ );
+};
+export default Course;
